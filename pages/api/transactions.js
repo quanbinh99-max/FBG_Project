5c4a9e2b91c9b6dad47aa60a033d04b94fcdba2d
tuongvyluan <87056936+tuongvyluan@users.noreply.github.com>
2022-05-08 02:16:35 +0700

    Put transaction API

--- pages/api/transactions.js
+++ pages/api/transactions.js
@@ -27,13 +27,13 @@ export default async function handler(req, res) {
 				}))
 			);
 			if (transaction != null) {
-				return res.status(404).json({ message: 'Mỗi người chỉ được mua một vé!' });
+				return res.status(400).json({ message: 'Mỗi người chỉ được mua một vé!' });
 			}
 			if (participant == null) {
-				return res.status(404).json({ message: 'Phải đăng ký trước khi mua vé!' });
+				return res.status(400).json({ message: 'Phải đăng ký trước khi mua vé!' });
 			}
 			if (ticketDetail == null) {
-				return res.status(404).json({ message: 'Vé không hợp lệ!' });
+				return res.status(400).json({ message: 'Vé không hợp lệ!' });
 			}
 			const status = StatusEnum.PENDING;
 			const updated_at = moment().format('YYYY-MM-DD HH:mm:ss');
@@ -46,6 +46,15 @@ export default async function handler(req, res) {
 				updated_at
 			});
 			return res.status(200).json({ message: 'Đăng ký mua vé thành công' });
+		} else if (req.method == 'PUT') {
+			const { participant_id } = req.body;
+			await Transaction.updateOne(
+				{ participant_id: participant_id },
+				{
+					paymentStatus: 'Đã thanh toán'
+				}
+			);
+			return res.status(200).json({ message: 'Thanh toán thành công' });
 		}
 	} catch (e) {
 		console.log(e.message);
