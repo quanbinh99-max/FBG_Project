f844ee4d0daebcd021c40a951cdeb94c4d4af0a3
HuyGiap <giap.nguyenhuy2102@gmail.com>
2022-05-09 23:48:07 +0700

    layout FBG

--- Components/FBG.js
+++ Components/FBG.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Section from "./Section";
+import Content from "./Content";
 
 function FBG(props) {
   return (
     <div>
       <Header></Header>
       <Section></Section>
+      <Content></Content>
     </div>
   );
 }
