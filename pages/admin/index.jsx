ce21c04ac64d7f21d7de1dde5bbcd6ee12239594
HuyGiap <giap.nguyenhuy2102@gmail.com>
2022-05-13 21:29:33 +0700

    update

---
+++ pages/admin/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Admin from "../../Components/admin/index";
+function index(props) {
+  return (
+    <div>
+      <Admin></Admin>
+    </div>
+  );
+}
+export default index;
