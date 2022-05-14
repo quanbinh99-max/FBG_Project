3722a339be31549e44f678131a25e16fb94ce1e7
tuongvyluan <87056936+tuongvyluan@users.noreply.github.com>
2022-05-14 18:05:55 +0700

    Update put transaction button

--- Components/Admin/index.js
+++ Components/Admin/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import apiClient from '../../util/http-common';
 import { Layout, Menu } from 'antd';
 import Participants from './participants';
 import Transactions from './transactions';
