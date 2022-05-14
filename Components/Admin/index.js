745c0493d5d0174c8454f228579b9d9271e611a7
HuyGiap <giap.nguyenhuy2102@gmail.com>
2022-05-14 20:49:05 +0700

    fix

--- Components/Admin/index.js
+++ Components/Admin/index.js
@@ -1,111 +1,111 @@
-import React, { useEffect, useState } from 'react';
-import { Layout, Menu } from 'antd';
-import Participants from './participants';
-import Transactions from './transactions';
-import Conten from './content';
+import React, { useEffect, useState } from "react";
+import { Layout, Menu } from "antd";
+import Participants from "./participants";
+import Transactions from "./transactions";
+import Confirm from "./confirm";
 import {
-	DesktopOutlined,
-	PieChartOutlined,
-	FileOutlined,
-	TeamOutlined,
-	UserOutlined
-} from '@ant-design/icons';
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 const { Header, Content, Footer, Sider } = Layout;
 
 function Index(props) {
-	const [tab, setTab] = useState(1);
+  const [tab, setTab] = useState(1);
 
-	function getItem(label, key, icon, children) {
-		return {
-			key,
-			icon,
-			children,
-			label,
-			onClick: () => {
-				setTab(Number(key));
-			}
-		};
-	}
+  function getItem(label, key, icon, children) {
+    return {
+      key,
+      icon,
+      children,
+      label,
+      onClick: () => {
+        setTab(Number(key));
+      },
+    };
+  }
 
-	const items = [
-		getItem('Participants', '1', <PieChartOutlined />),
-		getItem('Transactions', '2', <DesktopOutlined />)
-	];
+  const items = [
+    getItem("Participants", "1", <PieChartOutlined />),
+    getItem("Transactions", "2", <DesktopOutlined />),
+  ];
 
-	// const items = [UserOutlined, UserOutlined].map((icon, index) => ({
-	//   key: String(index + 1),
-	//   icon: React.createElement(icon),
-	//   label: `nav ${index + 1}`,
-	//   onClick: () => {
-	//     setTab(index);
-	//   },
-	// }));
+  // const items = [UserOutlined, UserOutlined].map((icon, index) => ({
+  //   key: String(index + 1),
+  //   icon: React.createElement(icon),
+  //   label: `nav ${index + 1}`,
+  //   onClick: () => {
+  //     setTab(index);
+  //   },
+  // }));
 
-	console.log(tab);
+  console.log(tab);
 
-	return (
-		<div>
-			{' '}
-			<Layout hasSider>
-				<Sider
-					style={{
-						overflow: 'auto',
-						height: '100vh',
-						position: 'fixed',
-						left: 0,
-						top: 0,
-						bottom: 0
-					}}
-				>
-					<div className="logo" />
-					<Menu
-						theme="dark"
-						mode="inline"
-						defaultSelectedKeys={['4']}
-						items={items}
-					/>
-				</Sider>
-				<Layout
-					className="site-layout"
-					style={{
-						marginLeft: 200
-					}}
-				>
-					<Header
-						className="site-layout-background"
-						style={{
-							padding: 0
-						}}
-					/>
-					<Content
-						style={{
-							margin: '24px 16px 0',
-							overflow: 'initial'
-						}}
-					>
-						<div
-							className="site-layout-background"
-							style={{
-								padding: 24,
-								textAlign: 'center'
-							}}
-						>
-							{tab === 1 && <Participants></Participants>}
-							{tab === 2 && <Transactions></Transactions>}
-						</div>
-					</Content>
-					<Footer
-						style={{
-							textAlign: 'center'
-						}}
-					>
-						<Conten></Conten>
-						Ant Design ©2018 Created by Ant UED
-					</Footer>
-				</Layout>
-			</Layout>
-		</div>
-	);
+  return (
+    <div>
+      {" "}
+      <Layout hasSider>
+        <Sider
+          style={{
+            overflow: "auto",
+            height: "100vh",
+            position: "fixed",
+            left: 0,
+            top: 0,
+            bottom: 0,
+          }}
+        >
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["4"]}
+            items={items}
+          />
+        </Sider>
+        <Layout
+          className="site-layout"
+          style={{
+            marginLeft: 200,
+          }}
+        >
+          <Header
+            className="site-layout-background"
+            style={{
+              padding: 0,
+            }}
+          />
+          <Content
+            style={{
+              margin: "24px 16px 0",
+              overflow: "initial",
+            }}
+          >
+            <div
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                textAlign: "center",
+              }}
+            >
+              {tab === 1 && <Participants></Participants>}
+              {tab === 2 && <Transactions></Transactions>}
+            </div>
+          </Content>
+          <Footer
+            style={{
+              textAlign: "center",
+            }}
+          >
+            <Confirm></Confirm>
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
+    </div>
+  );
 }
 
 export default Index;
