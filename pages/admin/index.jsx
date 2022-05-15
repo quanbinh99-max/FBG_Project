1089088ebb26cef9a1edd595604d978e5e7b4aaa
HuyGiap <giap.nguyenhuy2102@gmail.com>
2022-05-15 20:10:50 +0700

    secretkey admin page

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Admin from "../../Components/Admin/index";
-function index(props) {
+import apiClient from "../../util/http-common";
+import { useRouter } from "next/router";
+
+function Index(props) {
+  const router = useRouter();
+  const [login, setLogin] = useState(false);
+  console.log(router.query.key);
+  useEffect(() => {
+    const secretKey = async () => {
+      try {
+        const response = await apiClient.post("/key", {
+          key: router.query.key,
+        });
+        if (response.status === 200) {
+          setLogin(true);
+        } else {
+          setLogin(false);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    secretKey();
+  }, []);
+
   return (
     <div>
-      <Admin></Admin>
+      {login === true ? <Admin></Admin> : <h1>Bạn không phải Admin</h1>}
     </div>
   );
 }
 
-export default index;
+export default Index;
