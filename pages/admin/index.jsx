7f183143cc2d66b0cbfecc5f298b35b36ca6ce62
HuyGiap <giap.nguyenhuy2102@gmail.com>
2022-05-15 20:39:02 +0700

    git commit -m

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -4,30 +4,31 @@ import apiClient from "../../util/http-common";
 import { useRouter } from "next/router";
 
 function Index(props) {
-  const router = useRouter();
-  const [login, setLogin] = useState(false);
-  console.log(router.query.key);
-  useEffect(() => {
-    const secretKey = async () => {
-      try {
-        const response = await apiClient.post("/key", {
-          key: router.query.key,
-        });
-        if (response.status === 200) {
-          setLogin(true);
-        } else {
-          setLogin(false);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    secretKey();
-  }, []);
+  // const router = useRouter();
+  // const [login, setLogin] = useState(false);
+  // console.log(router.query.key);
+  // useEffect(() => {
+  //   const secretKey = async () => {
+  //     try {
+  //       const response = await apiClient.post("/key", {
+  //         key: router.query.key,
+  //       });
+  //       if (response.status === 200) {
+  //         setLogin(true);
+  //       } else {
+  //         setLogin(false);
+  //       }
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+  //   secretKey();
+  // }, []);
 
   return (
     <div>
-      {login === true ? <Admin></Admin> : <h1>Bạn không phải Admin</h1>}
+      <Admin></Admin>
+      {/* {login === true ? <Admin></Admin> : <h1>Bạn không phải Admin</h1>} */}
     </div>
   );
 }
